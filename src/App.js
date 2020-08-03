bb94eb915569ed05c88d437736da4347e38a4d92
Hassan <hassan.shokoohimatin@gmail.com>
2020-08-04 01:53:40 +0430

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends Component{
     render(){
         return(
             <div className="App">
-                ReactApp
+                <div className="card card-body text-danger text-center">
+                    React App
+                </div>
             </div>
         )
     }
